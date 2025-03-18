e69b96ba4124721a9f915f6a5c78194adc5c5127
Manuel Louie Gayao <louiemanuel238@gmail.com>
2025-03-18 15:18:37 -0600

    fix map and itinerary layout

--- src/Components/Itinerary/Itinerary.jsx
+++ src/Components/Itinerary/Itinerary.jsx
@@ -1,36 +1,52 @@
-import React, { useState } from 'react';
 
-const Itinerary = ({ data }) => {
-    
+import {Typography, Box, List, ListItemButton, ListItemText, Collapse} from '@mui/material'
+import React, { useState, Fragment } from 'react';
+import './Itinerary.scss'
+const Itinerary = ({data}) => {
+  
     const days = data.Activities;
-    console.log(days)
     const [filteredDays, setFilteredDays] = useState(days);
 
     const filterByDate = (date) => {
         const filtered = days.filter((activity) => activity.day === date);
         setFilteredDays(filtered);
     };
+    const [openIndex, setOpenIndex] = useState(null);
+
+    const handleClick = (index) => {
+        setOpenIndex(openIndex === index ? null : index);
+    };
 
     return (
-        <div>
-            {days.map((day)=> (<div>{day.day}</div>))}
-            <h1>{data.Title}</h1>
-            <p>{data.NoteForItinerary}</p>
-            
-                <div>
-                    <h2>{filteredDays.day}</h2>
-                    <p>{filteredDays.noteForTheDay}</p>
-{/* 
-                    {filteredDays.activity.map((act, index) => (
-                        <div key={index}>
-                            <h3>{act.place}</h3>
-                            <p>{act.description}</p>
-                            <p>Location: {act.location.lat}, {act.location.lng}</p>
-                        </div>
-                    ))} */}
-                </div>
-            
-        </div>
+        <section className='itinerary-panel'>
+            <Box className="itinerary-panel__body">
+                <ul className='itinerary-list'>
+                    {filteredDays.map((day, index) => (
+                        <li key={index} className='itinerary-list__item'>
+                            <ListItemButton  onClick={() => handleClick(index)}>
+                                <div className='travel-day'>
+                                    <h4 className='itinerary-list__date'>{new Date(day.day).toLocaleDateString('en-US', { month: 'long', day: 'numeric' })}</h4>
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M8 4L16 12L8 20" stroke="#008080" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                    <p className='itinerary-list__description'>{day.noteForTheDay}</p>
+                                </div>
+                            </ListItemButton>
+                            <Collapse in={openIndex === index} timeout="auto" unmountOnExit>
+                                <ul className='travel-day__collapse'>
+                                    {day.activity.map((act, actIndex) => (
+                                        <li key={actIndex} className='travel-day__places'>
+                                            <ListItemText primary={act.place} />
+                                            <ListItemText primary={act.description} />
+                                        </li>
+                                    ))}
+                                </ul>
+                            </Collapse>
+                        </li>
+                    ))}
+                </ul>
+            </Box>
+        </section>
     );
 };
 
