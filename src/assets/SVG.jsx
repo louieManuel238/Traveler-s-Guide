d24b4e7985f65afa1b708b0e3cf4859b42db35c8
Manuel Louie Gayao <louiemanuel238@gmail.com>
2025-03-28 20:22:54 -0600

    delete unsued assets

--- src/assets/SVG.jsx
+++
@@ -1,7 +0,0 @@
-
-const ArrowIcon = () => 
-<svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" enableBackground="new 0 0 26 26" height="26px" id="Layer_1" version="1.1" viewBox="0 0 26 26" width="26px" xmlSpace="preserve"><g>
-    <polygon fill="#231F20" points="0.046,2.582 2.13,0.498 12.967,11.334 23.803,0.498 25.887,2.582 12.967,15.502  "/>
-    <polygon fill="#231F20" points="0.046,13.582 2.13,11.498 12.967,22.334 23.803,11.498 25.887,13.582 12.967,26.502  "/></g></svg>
-
-export {ArrowIcon}
